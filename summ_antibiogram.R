3c6ac4e575f8f3cddcbbd242ca5db9937a11e0a0
Iana Amke <ianaamke@gmail.com>
2022-06-26 23:30:42 +0100

    Load antibiogram data and filter for fosfomycin

--- summ_antibiogram.R
+++ summ_antibiogram.R
@@ -1,2 +1,13 @@
+library(here)
 library(dplyr)
 library(ggplot2)
+
+# load antibiogram data
+antibiogram_data <- read.delim(file = "antibiogram_combined.tsv", header = TRUE, sep = "\t")
+
+# filter for fosfomycin 
+fos_antibiogram <- antibiogram_data %>%
+  filter(Antibiotic == "fosfomycin")
+
+
+  
