2ed49a5656a2f814dbb5ea82b6314114331588e4
Iana Amke <ianaamke@gmail.com>
2022-10-03 12:29:59 +0300

    Add K.pneumoniae fosfomycin EUCAST ECOFF cut-offs

--- r_scripts/summ_antibiogram.R
+++ r_scripts/summ_antibiogram.R
@@ -27,8 +27,9 @@ summ_dataset <- fos_antibiogram %>%
 # summary plot of total sample count by dataset
 fos_antibiogram %>%
   group_by(index) %>%
-  ggplot(aes(x = index, fill = Laboratory.Typing.Method)) +
-  geom_bar(alpha = 0.8) +
+  ggplot(aes(x = index)) +
+  geom_bar(fill = "#ff8000", alpha = 0.6) +
+  geom_text(aes(label = ..count..), stat = "count", nudge_y = 60) +
   labs(title = "Total sample count per dataset", y = "Total sample count", fill = "Lab typing method") +
   scale_fill_brewer(palette = "Accent") + theme(axis.title.x = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
 
@@ -46,12 +47,16 @@ summ_test_std <- fos_antibiogram %>%
   rename(Dataset = index, Total = n)
 
 # summary plots of total sample counts vs measurements for different lab typing methods  
+# ECOFF cut-offs used due to there being no K. pneumoniae breakpoints for fosfomycin
 # Disk diffusion
 fos_antibiogram %>%
   group_by(Laboratory.Typing.Method) %>%
   filter(Laboratory.Typing.Method == "Disk diffusion") %>%
   ggplot(aes(Measurement, fill = index)) + geom_bar() + 
-  labs(title = "Disk Diffusion Measurements", x = "Measurement (mm)", y = "Frequency", fill = "Dataset") 
+  # ECOFF cut-off of 15 mm
+  geom_vline(aes(xintercept = 15, linetype = "EUCAST"), alpha = 0.6, color = "black") +
+  labs(title = "Disk Diffusion Measurements", x = "Measurement (mm)", y = "Frequency", fill = "Dataset", linetype = "Disk diffusion \nECOFF cut-off") +
+  scale_linetype_manual(values = "dotted")
 
 # summary of MIC measurements
 # MIC agar dilution 
@@ -59,21 +64,27 @@ fos_antibiogram %>%
   group_by(Laboratory.Typing.Method) %>%
   filter(Laboratory.Typing.Method == "MIC (agar dilution)") %>%
   ggplot(aes(x = factor(Measurement), fill = index)) + geom_bar() + 
-  labs(title = "MIC Agar Dilution Measurements", x = "Measurement (mg/L)", y = "Frequency", fill = "Dataset") 
+  # ECOFF cut-off of 128 mg/L
+  geom_vline(aes(xintercept = 7, linetype = "EUCAST"), alpha = 0.6, color = "black") +
+  labs(title = "MIC Agar Dilution Measurements", x = "Measurement (mg/L)", y = "Frequency", fill = "Dataset", linetype = "MIC ECOFF cut-off") +
+  scale_linetype_manual(values = "dotted")
 
 # MIC broth dilution 
 fos_antibiogram %>%
   group_by(Laboratory.Typing.Method) %>%
   filter(Laboratory.Typing.Method == "MIC (broth dilution)") %>%
   ggplot(aes(x = factor(Measurement), fill = index)) + geom_bar() + 
-  labs(title = "MIC Broth Dilution Measurements", x = "Measurement (mg/L)", y = "Frequency", fill = "Dataset") + 
+  # ECOFF cut-off of 128 mg/L
+  geom_vline(aes(xintercept = 6, linetype = "EUCAST"), alpha = 0.6, color = "black") +
+  labs(title = "MIC Broth Dilution Measurements", x = "Measurement (mg/L)", y = "Frequency", fill = "Dataset", linetype = "MIC ECOFF cut-off") +
+  scale_linetype_manual(values = "dotted") +
   scale_x_discrete(limits = factor(2^seq(2, 9, by = 1)))
 
 
 # plot lab testing methods for one dataset against the other datasets
 
 # create colour variable 
-cols <- c("other_dataset" = "purple", "current_dataset" = "cyan1")
+cols <- c("other dataset" = "purple", "current dataset" = "cyan1")
 
 # disk diffusion
 dataset_disk_diff <- fos_antibiogram %>%
@@ -84,9 +95,9 @@ dataset_disk_diff_noindex <- dataset_disk_diff %>%
   ungroup() %>%
   select(Sample.Name:Laboratory.Typing.Method.Version.or.Reagent)
 
-ggplot(dataset_disk_diff, aes(Measurement)) + geom_bar(data = dataset_disk_diff_noindex, aes(fill = "other_dataset"), alpha = 0.5) +
-  geom_bar(aes(fill = "current_dataset")) + facet_wrap(~ index, ncol = 1) + 
-  labs(title = "Fosfomycin Disk Diffusion", x = "Measurement (mm)", fill = "Dataset")
+ggplot(dataset_disk_diff, aes(Measurement)) + geom_bar(data = dataset_disk_diff_noindex, aes(fill = "other dataset"), alpha = 0.5) +
+  geom_bar(aes(fill = "current dataset")) + facet_wrap(~ index, ncol = 1) + 
+  labs(title = "Fosfomycin Disk Diffusion Measurements per Dataset", x = "Measurement (mm)", fill = "Dataset")
 
 # MIC (agar dilution)
 dataset_MIC_agar <- fos_antibiogram %>%
@@ -97,9 +108,9 @@ dataset_MIC_agar_noindex <- dataset_MIC_agar %>%
   ungroup() %>%
   select(Sample.Name:Laboratory.Typing.Method.Version.or.Reagent)
 
-ggplot(dataset_MIC_agar, aes(factor(Measurement))) + geom_bar(data = dataset_MIC_agar_noindex, aes(fill = "other_dataset"), alpha = 0.5) +
-  geom_bar(aes(fill = "current_dataset")) + facet_wrap(~ index, ncol = 1) + 
-  labs(title = "Fosfomycin MIC Agar Dilution", x = "Measurement (mg/L)", fill = "Dataset") 
+ggplot(dataset_MIC_agar, aes(factor(Measurement))) + geom_bar(data = dataset_MIC_agar_noindex, aes(fill = "other dataset"), alpha = 0.5) +
+  geom_bar(aes(fill = "current dataset")) + facet_wrap(~ index, ncol = 1) + 
+  labs(title = "Fosfomycin MIC Agar Dilution Measurements per Dataset", x = "Measurement (mg/L)", fill = "Dataset") 
 
 # MIC (broth dilution)
 dataset_MIC_broth <- fos_antibiogram %>%
@@ -110,7 +121,7 @@ dataset_MIC_broth_noindex <- dataset_MIC_broth %>%
   ungroup() %>%
   select(Sample.Name:Laboratory.Typing.Method.Version.or.Reagent)
 
-ggplot(dataset_MIC_broth, aes(factor(Measurement))) + geom_bar(data = dataset_MIC_broth_noindex, aes(fill = "other_dataset"), alpha = 0.5) +
-  geom_bar(aes(fill = "current_dataset")) + facet_wrap(~ index, ncol = 1) + 
-  labs(title = "Fosfomycin MIC Broth Dilution", x = "Measurement (mg/L)", fill = "Dataset") +
+ggplot(dataset_MIC_broth, aes(factor(Measurement))) + geom_bar(data = dataset_MIC_broth_noindex, aes(fill = "other dataset"), alpha = 0.5) +
+  geom_bar(aes(fill = "current dataset")) + facet_wrap(~ index, ncol = 1) + 
+  labs(title = "Fosfomycin MIC Broth Dilution Measurements per Dataset", x = "Measurement (mg/L)", fill = "Dataset") +
   scale_x_discrete(limits = factor(2^seq(3, 9, by = 1)))
