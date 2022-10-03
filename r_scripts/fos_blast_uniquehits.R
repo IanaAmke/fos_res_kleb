892af8988cf1b5da99a082727a0fa5683a86de2a
Iana Amke <ianaamke@gmail.com>
2022-10-03 12:42:36 +0300

    Add labels and EUCAST ECOFF cut-offs

--- r_scripts/fos_blast_uniquehits.R
+++ r_scripts/fos_blast_uniquehits.R
@@ -39,7 +39,8 @@ fos_fcyn_uniquehits <- inner_join(fos_antibiogram, fos_uniquehits, by = c("Sampl
 # create dataframe with contig.number column that counts number of fos genes in a genome
 fos_fcyn_uniquehits <- fos_fcyn_uniquehits %>%
   group_by(strain) %>%
-  mutate(contig.number = n())
+  add_count() %>%
+  rename(contig.number = n) 
 
 # relocate strain after index and contig.number after contig
 fos_fcyn_uniquehits <- fos_fcyn_uniquehits %>%
@@ -61,8 +62,12 @@ fos_fcyn_uniquehits_samecontig_filtered <- subset(fos_fcyn_uniquehits_samecontig
 # delete contig.number and n columns
 fos_fcyn_uniquehits_samecontig_filtered <- subset(fos_fcyn_uniquehits_samecontig_filtered, select = -c(n))
 
-# rename fos_fcyn_uniquehits_samecontig_filtered to fos_fcyn_uniquehits
-fos_fcyn_uniquehits <- fos_fcyn_uniquehits_samecontig_filtered
+# rename fos_fcyn_uniquehits_samecontig_filtered to fos_fcyn_uniquehits and count gene copies
+fos_fcyn_uniquehits <- fos_fcyn_uniquehits_samecontig_filtered %>%
+  group_by(strain) %>%
+  add_count() %>%
+  mutate(contig.number = paste(n)) %>%
+  select(- n)
 
 # save fos_fcyn_uniquehits to tsv file
 #write_delim(fos_fcyn_uniquehits, file = "fosgenes_antibiogram_uniqueHits.tsv", delim = "\t")
@@ -72,6 +77,8 @@ fos_fcyn_uniquehits %>%
   ggplot(aes(fos.gene)) + geom_bar(fill = "#0000CC") +  
   labs(title = "Fosfomycin Resistance Genes Distribution",x = "fos gene", 
        y = "Count", fill = "Fosfomycin resistance genes") + 
+  # add labels on top of bars
+  geom_text(aes(label = ..count..), stat = "count", nudge_y = 12) +
   theme(axis.title.x = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
 
 # frequency table of fos gene distribution
@@ -89,23 +96,32 @@ fos_fcyn_uniquehits %>%
 # MIC broth dilution
 fos_fcyn_uniquehits %>%
   filter(Laboratory.Typing.Method == "MIC (broth dilution)") %>%
-  ggplot(aes(factor(Measurement), fill = fos.gene)) + geom_bar() +  
+  ggplot(aes(factor(Measurement), fill = fos.gene)) + geom_bar() +
+  # ECOFF cut-off of 128 mg/L
+  geom_vline(aes(xintercept = 4, linetype = "EUCAST"), alpha = 0.6, color = "black") +
   labs(title = "Fosfomycin Resistance Genes Distribution for MIC (Broth Dilution)",x = "MIC (broth dilution) measurement (mg/L)", 
-       y = "Count", fill = "Fosfomycin resistance genes") 
+       y = "Count", fill = "Fosfomycin resistance genes", linetype = "MIC ECOFF cut-off") +
+  scale_linetype_manual(values = "dotted") 
 
 # MIC agar dilution
 #fos_fcyn_uniquehits %>%
   #filter(Laboratory.Typing.Method == "MIC (agar dilution)") %>%
   #ggplot(aes(factor(Measurement), fill = fos.gene)) + geom_bar() +  
-  #labs(title = "Fosfomycin Resistance Genes Distribution",x = "MIC (agar dilution) measurement (mg/L)", 
-  #y = "Count", fill = "Fosfomycin resistance genes") 
+  # ECOFF cut-off of 128 mg/L
+  #geom_vline(aes(xintercept = 7, linetype = "EUCAST"), alpha = 0.6, color = "black") +
+  #labs(title = "Fosfomycin Resistance Genes Distribution for MIC (Agar Dilution)",x = "MIC (agar dilution) measurement (mg/L)", 
+       #y = "Count", fill = "Fosfomycin resistance genes", linetype = "MIC ECOFF cut-off") +
+  #scale_linetype_manual(values = "dotted")
 
 # Disk diffusion
 fos_fcyn_uniquehits %>%
   filter(Laboratory.Typing.Method == "Disk diffusion") %>%
-  ggplot(aes(factor(Measurement), fill = fos.gene)) + geom_bar() +  
+  ggplot(aes(Measurement, fill = fos.gene)) + geom_bar() + 
+  # ECOFF cut-off of 15 mm
+  geom_vline(aes(xintercept = 15, linetype = "EUCAST"), alpha = 0.6, color = "black") +
   labs(title = "Fosfomycin Resistance Genes Distribution for Disk Diffusion",x = "Disk diffusion measurement (mm)", 
-       y = "Count", fill = "Fosfomycin resistance genes") 
+       y = "Count", fill = "Fosfomycin resistance genes", linetype = "Disk diffusion \nECOFF cut-off") +
+  scale_linetype_manual(values = "dotted")
 
 # bar plots showing total number of samples with fos genes per lab typing method and dataset
 # MIC broth dilution
@@ -134,6 +150,7 @@ fos_fcyn_uniquehits %>%
 
 # UPSET PLOTS
 # MIC broth violin plots for individual res patterns
+# ECOFF cut-offs added to infer phenotype
 # filter for MIC broth
 micbroth_fos_fcyn_uniquehits <- fos_fcyn_uniquehits %>%
   filter(Laboratory.Typing.Method == "MIC (broth dilution)")
@@ -142,9 +159,30 @@ micbroth_fos_fcyn_uniquehits %>%
   group_by(Sample.Name, Measurement) %>% 
   summarize(gene_list = list(fos.gene)) %>%
   ggplot(aes(gene_list, Measurement)) + geom_violin() + geom_count() + 
-  labs(title = "Fos Gene Pattern for MIC (Broth Dilution)", x = "fos genes", y = "Fosfomycin MIC broth measurement (mg/L)", 
-       color = "fos genes", size = "Number of strains") + scale_x_upset() +
+  # ECOFF cut-off of 128 mg/L
+  geom_hline(aes(yintercept = 128, linetype = "EUCAST"), alpha = 0.6, color = "black") +
+  labs(title = "Fos Gene Pattern for MIC (Broth Dilution)", x = "fos genes", y = "Fosfomycin MIC broth measurement (mg/L)",
+       size = "Number of strains", linetype = "MIC ECOFF cut-off") +
+  scale_linetype_manual(values = "dotted") +
+  scale_x_upset() +
   theme_combmatrix(combmatrix.panel.line.size = 0.8)
+
+# MIC agar violin plots for individual res patterns
+# filter for MIC agar
+#micagar_fos_fcyn_uniquehits <- fos_fcyn_uniquehits %>%
+  #filter(Laboratory.Typing.Method == "MIC (agar dilution)")
+
+#micagar_fos_fcyn_uniquehits %>%
+  #group_by(Sample.Name, Measurement) %>% 
+  #summarize(gene_list = list(fos.gene)) %>%
+  #ggplot(aes(gene_list, Measurement)) + geom_violin() + geom_count() + 
+  # ECOFF cut-off of 128 mg/L
+  #geom_hline(aes(yintercept = 128, linetype = "EUCAST"), alpha = 0.6, color = "black") +
+  #labs(title = "Fos Gene Pattern for MIC (Agar Dilution)", x = "fos genes", y = "Fosfomycin MIC agar measurement (mg/L)",
+  #size = "Number of strains", linetype = "MIC ECOFF cut-off") +
+  #scale_linetype_manual(values = "dotted") +
+  #scale_x_upset() +
+  #theme_combmatrix(combmatrix.panel.line.size = 0.8)
 
 # Disk diffusion violin plots for individual res patterns
 # filter for disk diffusion
@@ -155,7 +193,11 @@ diskdiff_fos_fcyn_uniquehits %>%
   group_by(Sample.Name, Measurement) %>% 
   summarize(gene_list = list(fos.gene)) %>%
   ggplot(aes(gene_list, Measurement)) + geom_violin() + geom_count() + 
-  labs(title = "Fos Gene Pattern for Disk Diffusion", x = "fos genes", y = "Fosfomycin disk diffusion measurement (mm)", 
-       color = "fos genes", size = "Number of strains") + scale_x_upset() +
+  # ECOFF cut-off of 15 mm
+  geom_hline(aes(yintercept = 15, linetype = "EUCAST"), alpha = 0.6, color = "black") +
+  labs(title = "Fos Gene Pattern for Disk Diffusion", x = "fos genes", y = "Fosfomycin disk diffusion measurement (mm)",
+       size = "Number of strains", linetype = "Disk diffusion \nECOFF cut-off") +
+  scale_linetype_manual(values = "dotted") +
+  scale_x_upset() +
   theme_combmatrix(combmatrix.panel.line.size = 0.8)
   
