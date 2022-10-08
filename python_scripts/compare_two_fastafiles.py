cd57912cf6b2a6405f80ce85abb1a3864246364a
IanaAmke <74602057+IanaAmke@users.noreply.github.com>
2022-10-08 22:47:20 +0300

    Delete compare_two_fastafiles.py

--- python_scripts/compare_two_fastafiles.py
+++
@@ -1,7 +0,0 @@
-from difflib import Differ
-  
-with open('trial.fasta') as file_1, open('trial_2.fasta') as file_2:
-    differ = Differ()
-  
-    for line in differ.compare(file_1.readlines(), file_2.readlines()):
-        print(line)
